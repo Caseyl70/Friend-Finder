4fcb64e5b8718330d6d2bc7a30da2bbf0c0f35ec
Casey Levin <clevin8080@yahoo.com>
2019-11-04 17:51:05 -0700

    Found a friend

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,4 +3,30 @@ module.exports = function(app){
 app.get("/friends", function(req, res){
 res.json(friends);
 });
+app.post("/api/friends", function(req, res){
+    var user = req.body;
+    // console.log(user);
+    var bestFriend = {
+        name: "", 
+        photo: "", 
+        total: 1000,
+    };
+    friends.forEach(friend => {
+        var friendScores = friend.scores;
+        var userScores = user.scores;
+        var total = 0;
+        for (let i = 0; i < userScores.length; i++){
+            var difference = Math.abs(parseInt(friendScores[i])- parseInt(userScores[i]));
+            total += difference;
+        };
+        if (total < bestFriend.total){
+            bestFriend.name = friend.name;
+            bestFriend.photo = friend.photo;
+            bestFriend.total = total;
+        }
+    });
+    friends.push(user);
+    res.json(bestFriend);
+})
+
 }
